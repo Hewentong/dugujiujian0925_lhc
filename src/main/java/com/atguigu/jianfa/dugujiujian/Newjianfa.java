5d770fed9ba53778b1b33a95e3541bef4ea3cdd0
zhangchen <www.1838997924@qq.com>
2020-06-17 23:02:34 +0800

    newjianfa

--- src/main/java/com/atguigu/jianfa/dugujiujian/Newjianfa.java
+++ src/main/java/com/atguigu/jianfa/dugujiujian/Newjianfa.java
@@ -4,5 +4,10 @@ public class Newjianfa {
     public static void main(String[] args){
 
         System.out.println("新剑法!!!!");
+        System.out.println("新剑法!!!!");
+        System.out.println("新剑法!!!!");
+        System.out.println("新剑法!!!!");
+        System.out.println("新剑法!!!!");
+        System.out.println("新剑法!!!!");
     }
 }
