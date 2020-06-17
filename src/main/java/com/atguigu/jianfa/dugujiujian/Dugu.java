827dda4cc0c69a2d8922c6af32e110113c8f3942
zhangchen <www.1838997924@qq.com>
2020-06-17 22:14:53 +0800

    岳不群修改

--- src/main/java/com/atguigu/jianfa/dugujiujian/Dugu.java
+++ src/main/java/com/atguigu/jianfa/dugujiujian/Dugu.java
@@ -4,6 +4,10 @@ public class Dugu {
     public static void main(String[] args){
 
         System.out.println("jianfa dugujiujian!!!!");
+        System.out.println("jianfa dugujiujian!!!!");
+        System.out.println("jianfa dugujiujian!!!!");
+        System.out.println("jianfa dugujiujian!!!!");
+        System.out.println("jianfa dugujiujian!!!!");
     }
 
 }
